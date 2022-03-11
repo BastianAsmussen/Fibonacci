6356a92a2a9974f6123b3d3dfd790f1ae8e2570c
Bastian <76102128+BastianAsmussen@users.noreply.github.com>
2022-03-11 16:02:59 +0100

    Add files via upload

---
+++ src/tech/asmussen/Main.java
@@ -0,0 +1,85 @@
+package tech.asmussen;
+
+import java.math.BigInteger;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Main {
+
+    private static BigInteger[] fibonacciCache;
+
+    public static void main(String[] args) {
+
+        System.out.println("Use the --sequence argument to get the entire sequence up to your selected number.");
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean validNumber = false;
+        boolean sequence = args.length > 0 && args[0].equalsIgnoreCase("--sequence");
+
+        long n = 0;
+
+        do {
+
+            System.out.print("What number of the fibonacci sequence do you want? ");
+            String rawInput = scanner.nextLine();
+
+            try {
+
+                n = Long.parseLong(rawInput);
+                validNumber = true;
+
+                scanner.close();
+
+            } catch (NumberFormatException e) {
+
+                System.out.println("[ERROR]: Invalid number, please try again!");
+            }
+
+        } while (!validNumber);
+
+        System.out.println();
+
+        DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
+
+        fibonacciCache = new BigInteger[(int) n + 1];
+
+        final long START_TIME = System.currentTimeMillis();
+
+        fibonacci(n);
+
+        final double END_TIME = (System.currentTimeMillis() - START_TIME) / 1_000D;
+        final String[] TIME = String.valueOf(END_TIME).split("\\.");
+
+        StringBuilder fibonacciSequence = new StringBuilder();
+
+        if (sequence) {
+
+            for (long i = 0; i < n; i++) {
+
+                fibonacciSequence.append(decimalFormat.format(fibonacci(i))).append(", ");
+            }
+
+            fibonacciSequence.delete(fibonacciSequence.length() - 2, fibonacciSequence.length());
+
+        } else {
+
+            fibonacciSequence.append(fibonacci(n));
+        }
+
+        System.out.printf(((sequence) ? "Fibonacci Sequence:" : "Fibonacci Number:") + " %s.%n", fibonacciSequence);
+        System.out.printf("Time: %s second(s) and %s millisecond(s).%n", TIME[0], TIME[1]);
+    }
+    
+    private static BigInteger fibonacci(long n) {
+
+        if (n <= 1) return BigInteger.valueOf(n);
+        if (fibonacciCache[(int) n] != null) return fibonacciCache[(int) n];
+
+        BigInteger nthFibonacciNumber = fibonacci(n - 1).add(fibonacci(n - 2));
+
+        fibonacciCache[(int) n] = nthFibonacciNumber;
+
+        return nthFibonacciNumber;
+    }
+}
